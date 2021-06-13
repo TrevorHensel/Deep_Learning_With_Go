56c4e486c1f792a0a1165435feca85b00c5dd795
King-of-57 <henseltrevor@gmail.com>
2021-06-12 19:01:32 -0700

    Added more to the board

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -1,6 +1,8 @@
 import copy
 from dlgo.gotypes import Player
 from hotshot import stones
+from pickle import NONE
+from Carbon.Aliases import false
 
 class Move():
     def __init__(self, point=None, is_pass=False, is_resign=False):
@@ -38,3 +40,114 @@ class GoString():
         assert go_string.color == self.color
         combined_stones = self.stones | go_string.stones
         return GoString(self.color, combined_stones, (self.liberties | go_string.liberties) - combined_stones)
+    
+class Board():
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = ()
+        
+    def place_stone(self, player, point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point is None)
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties = []
+        
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            
+            neighbor_string = self._grid.get(neighbor)
+            
+            if neighbor_string is None:
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_opposite_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+                    
+        new_string = GoString(player, [point], liberties)
+        
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+            
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+            
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+            
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
+        
+        
+    def is_on_grid(self, point):
+        return 1 <= point.row <= self.num_rows and 1 <= point.col <= self.num_cols
+    
+    def get(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return NONE
+        return string.color
+    
+    def get_go_string(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return NONE
+        return string
+        
+    def _remove_string(self, string):
+        for point in string.stones:
+            for neighbor in point.neighbors():
+                neighbor_string = self._grid.get(neighbor)
+                
+                if neighbor_string is None:
+                    continue
+                
+                if neighbor_string is not string:
+                    neighbor_string.add_liberty(point)
+                    
+            self._grid[point] = None
+            
+class GameState():
+    def __init__(self, board, next_player, previous, move):
+        self.board = Board
+        self.next_player = next_player
+        self.previous_state = previous
+        self.last_move = move
+        
+    def apply_move(self, move):
+        if move.is_play:
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player, move.point)
+        else:
+            next_board = self.board
+            
+        return GameState(next_board, self.next_player, other, self, move)
+    
+    @classmethod
+    def new_game(cls, board_size):
+        if isinstance(board_size, int):
+            board_size = (board_size, board_size)
+            
+        board = Board(*board_size)
+        
+        return GameState(board, Player.black, None, None)
+    
+    def is_over(self):
+        if self.last_move is None:
+            return False
+        
+        if self.last_move.is_resign:
+            return True
+        
+        second_last_move = self.previous_state.last_move
+        
+        if second_last_move is None:
+            return False
+        
+        return self.last_move.is_pass and second_last_move.is_pass
